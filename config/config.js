0911b7e4947a0477c6f98c681da4aa465ee8f4f6
billhu422 <billhu422@126.com>
2017-08-04 17:54:29 +0800

    add dockerfile

--- config/config.js
+++ config/config.js
@@ -2,13 +2,13 @@ module.exports = {
     development: {
         app: {
             name: 'Passport SAML strategy example',
-            port: process.env.PORT || 3000
+            port: process.env.PORT || 9001
         },
         passport: {
             strategy: 'saml',
             saml: {
                 path: process.env.SAML_PATH || '/login/callback',
-                entryPoint: process.env.SAML_ENTRY_POINT || 'https://repo.ngboss.com:9443/samlsso',
+                entryPoint: process.env.SAML_ENTRY_POINT || 'https://124.251.62.217:9443/samlsso',
                 issuer: 'passport-saml',
                 identifierFormat:'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress',
                 cert: process.env.SAML_CERT || null,
